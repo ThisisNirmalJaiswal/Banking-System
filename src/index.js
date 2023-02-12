2dfab90482daaf0fdb93051545c65deaaadce1d6
sabiha <105931254+ThisisNirmalJaiswal@users.noreply.github.com>
2023-02-12 12:34:45 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const route = require("./routes/route")
+app.use(express.json());
+
+
+const URL = "mongodb+srv://nirmaljasval:8o1g7W6bqoshvXoN@cluster0.cv9nolo.mongodb.net/Banking-API";
+
+mongoose.connect(URL, {useNewUrlParser: true})
+.then(()=>{
+    console.log("huuu data base is connected")
+})
+.catch((err=>{console.log("mongo is disappointend " + err)}))
+
+app.use("/", route);
+
+app.listen(3000, ()=>{
+    console.log("Your app is  running on PORT 3000")
+})
